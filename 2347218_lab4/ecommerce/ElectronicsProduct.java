379d75d48f3fe3a0fa1e09da5f1beca159615eea
ChrisAlister2347218 <141411706+ChrisAlister2347218@users.noreply.github.com>
2023-11-28 22:31:18 +0530

    Add files via upload

---
+++ 2347218_lab4/ecommerce/ElectronicsProduct.java
@@ -0,0 +1,26 @@
+package ecommerce;
+
+public class ElectronicsProduct implements Product {
+    private String name;
+    private double price;
+
+    public ElectronicsProduct(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Electronics Product: " + name;
+    }
+}
---
+++ 2347218_lab4/ecommerce/Product.java
@@ -0,0 +1,7 @@
+package ecommerce;
+
+public interface Product {
+    String getName();
+    double getPrice();
+    String getDescription();
+}
